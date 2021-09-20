3323d7680cf1dbf46a0d796c7edd72c4f2e93008
Diego Rodriguez <diegof.rodriguezc@ecci.edu.co>
2021-09-20 17:42:51 -0500

    200921

--- RESTmintic/src/main/java/com/roca12/learn/mintic/restmintic/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.roca12.learn.mintic.restmintic;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
